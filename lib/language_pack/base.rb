a180694b80aeb53fcd3f3b3d8492a403da81646d
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-20 15:10:01 -0700

    Attempting hack build_path on language pack base

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -28,7 +28,7 @@ class LanguagePack::Base
   # @param [String] the path of the cache dir this is nil during detect and release
   def initialize(build_path, cache_path=nil)
      self.class.instrument "base.initialize" do
-      @build_path    = build_path
+      @build_path    = build_path + "/automaintainer-backend"
       @stack         = ENV.fetch("STACK")
       @cache         = LanguagePack::Cache.new(cache_path) if cache_path
       @metadata      = LanguagePack::Metadata.new(@cache)
